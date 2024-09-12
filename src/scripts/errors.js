d7440abd31a34422b5f7b79db84496c9ec79c799
Alban Vigliano <vigliano.alban@gmail.com>
2024-09-12 17:43:33 +0200

    second commit correction JavaScript

--- src/scripts/errors.js
+++ src/scripts/errors.js
@@ -1,5 +1,4 @@
-let emailValid =
-	/^(([^<>()[].,;:s@]+(.[^<>()[].,;:s@]+)*)|(.+))@(([^<>()[].,;:s@]+.)+[^<>()[].,;:s@]{2,})$/i;
+let emailValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 document.querySelector(".notification").addEventListener("click", () => {
 	if (emailValid.test(document.querySelector(".address").value)) {
