4a325264e14a611d6240bb1d81cd925b8fab4071
Alban Vigliano <vigliano.alban@gmail.com>
2024-09-13 17:39:43 +0200

    Third commit correction JavaScript

--- src/scripts/errors.js
+++ src/scripts/errors.js
@@ -2,7 +2,13 @@ let emailValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 document.querySelector(".notification").addEventListener("click", () => {
 	if (emailValid.test(document.querySelector(".address").value)) {
+		document.querySelector(".invalidmail").style.visibility = "hidden";
 	} else {
 		document.querySelector(".invalidmail").style.visibility = "visible";
 	}
+});
+
+document.querySelector(".notification").addEventListener("click", () => {
+	let init = document.querySelector(".address");
+	init.value = "";
 });
